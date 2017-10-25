4574e6bca7cb5897f984630be8055d0f310ef478
B1 <B1@B132>
2017-10-25 13:50:22 +0800

    Commit 1

---
+++ src/f9/F9.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package f9;
+
+/**
+ *
+ * @author B1
+ */
+public class F9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello - Main");
+        System.out.println("Hi - Second");
+    }
+    
+}
